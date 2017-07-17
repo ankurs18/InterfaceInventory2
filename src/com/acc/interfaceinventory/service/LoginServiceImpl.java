b5aaff5896fda7f4721c0c9d58347f07bac9f08c
dipak.p.patil <dipak.p.patil@P5-GL9K882.dir.svc.accenture.com>
2017-07-17 12:14:52 +0530

    vcv

--- src/com/acc/interfaceinventory/service/LoginServiceImpl.java
+++ src/com/acc/interfaceinventory/service/LoginServiceImpl.java
@@ -1,7 +1,8 @@
 package com.acc.interfaceinventory.service;
 
-import com.acc.interfaceinventory.Beans.LoginBean;
+
 import com.acc.interfaceinventory.dao.LoginDAO;
+import com.acc.interfaceinventory.entity.LoginBean;
 
 public class LoginServiceImpl {
 	public LoginBean checkLogin(String username, String password){
